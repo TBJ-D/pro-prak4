eab47030868d89e4e48d9d7ed0f3ae07b6383f95
Tommie taakstraf <336217@student.mboutrecht.nl>
2023-04-11 10:05:12 +0200

    removed username from users table DB

--- db/database.sql
+++ db/database.sql
@@ -9,7 +9,6 @@ email varchar(255) primary key not null unique
 create table users
 (
 userId int primary key not null auto_increment,
-username varchar(255) not null,
 password varchar(255) not null,
 email varchar(255)
 );
