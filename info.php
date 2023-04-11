4fdfc0ba9799e6fe44032145179befcd5dc56b73
KeremTekinnn <335631@student.mboutrecht.nl>
2023-04-11 09:21:16 +0200

    started work info.php

---
+++ info.php
@@ -0,0 +1,12 @@
+
+<body>
+    <?php
+        include_once("header.php");
+    ?> 
+    <main id="info">
+        <div class="top">
+            <img src="media/home_banner.png" alt="iphone14">
+        </div>
+    </main>
+
+</body>
