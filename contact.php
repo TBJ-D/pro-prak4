2e91cc3521e1c1435df8762655c8fb29da81268e
Igone van_der_Kley <igone@vdkley.nl>
2023-04-16 22:29:26 +0200

    form styling

--- contact.php
+++ contact.php
@@ -1,30 +1,38 @@
 <head>
     <link rel="stylesheet" href="/scss/contact.scss">
 </head>
+
+
 <body>
     <?php
     include_once("header.php");
     ?>
 
-<div class="flex">
-     <div class="form-content">
-   
-   
-    <form action="">
+    <div class="form-container">
+        <div class="form-background">
 
-       <h1>Contact</h1>  
-        <label for="email">Email</label><br>
-        <input type="text" id=email><br>
-        <label for="onderwerp">Onderwerp</label><br>
-        <input type="text" id="onderwerp"><br>
-        <label for="inhoud">Inhoud</label><br>
-        <input type="text" id="inhoud" style="height: 50px; width: 200px; padding: 10px;">><br>
 
-        <input type="radio" id="radio">Inschrijven voor nieuwsbrief<br>
-        <br><br>
-        <input type="submit" value="Verstuur" id="submit">
-    </form>
-</div>
-</div>
+            <form action="">
+
+                <h1>Contact</h1>
+                <label for="email">Email</label><br>
+                <input type="text" id=email placeholder="example@gmail.com"><br>
+                <label for="onderwerp">Onderwerp</label><br>
+                <input type="text" id="onderwerp"><br>
+                <label for="inhoud">Inhoud</label><br>
+                <textarea type="text" id="inhoud"></textarea><br>
+
+
+                <input type="radio" id="radio">
+                <label for="radio">Inschrijven voor nieuwsbrief</label>
+
+                <br><br>
+
+                <div class="submit-container">
+                    <input type="submit" value="Verstuur" id="submit">
+                </div>
+            </form>
+        </div>
+    </div>
 
 </body>
