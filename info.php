028d812307fa7f252ac53ccec21bd85a83d6df2b
Shahed Amer <chanyeol6521354@gmail.com>
2023-04-17 09:06:05 +0200

    info page is gemaakt

--- info.php
+++ info.php
@@ -1,12 +1,29 @@
-
 <body>
     <?php
-        include_once("header.php");
-    ?> 
+    include_once("header.php");
+    ?>
     <main id="info">
         <div class="top">
             <img src="media/home_banner.png" alt="iphone14">
         </div>
     </main>
+    <div class="banner">
+        <img src="" alt="">
+        <h1>iPhone 14 pro</h1>
+    </div>
+    <div class="s1">
+        <div class="info-img">
+            <img src="" alt="">
+            <h2>Informatievak</h2>
+            <div class="btns">
+                <button class="specs-page">
+                    <a href="specs.php">Specs</a>
+                </button>
+                <button class="promotie-page">
+                    <a href="promotie.php">Promotie</a>
+                </button>
+            </div>
+        </div>
+    </div>
 
 </body>
