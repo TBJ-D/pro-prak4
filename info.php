d26e4ed5f203d6cc800be3d632d335d72ca09d9b
Shahed Amer <chanyeol6521354@gmail.com>
2023-04-17 09:19:01 +0200

    info html is af

--- info.php
+++ info.php
@@ -2,25 +2,31 @@
     <?php
     include_once("header.php");
     ?>
-    <main id="info">
+    <!-- <main id="info">
         <div class="top">
             <img src="media/home_banner.png" alt="iphone14">
         </div>
-    </main>
+    </main> -->
     <div class="banner">
-        <img src="" alt="">
+        <img src="/media/image 1.png" alt="iphone-img">
         <h1>iPhone 14 pro</h1>
     </div>
     <div class="s1">
         <div class="info-img">
             <img src="" alt="">
             <h2>Informatievak</h2>
+            <p class="text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi praesentium eius voluptatum tenetur nam? Nam, id. Dolore quia quis sunt laudantium, beatae saepe deleniti, deserunt dolor aperiam nulla voluptatibus omnis!
+                Minus, magni quod vel eos praesentium magnam commodi enim adipisci itaque dolorem, placeat, beatae explicabo voluptatum alias sit dignissimos aperiam ullam! Obcaecati reiciendis at libero voluptate. Temporibus optio quam impedit?
+                Sit, quo nisi perferendis minima nihil iusto vitae nemo architecto voluptatum ducimus unde labore similique neque illum nostrum. Consectetur inventore voluptas quasi quisquam, unde saepe provident dolores id vel dolor?
+                Necessitatibus nihil deserunt fugit laborum vero laboriosam suscipit voluptates neque provident dolorum sint ipsum repellendus consequuntur debitis blanditiis iusto incidunt voluptas maiores et, nostrum explicabo voluptate itaque pariatur reprehenderit? Voluptas.
+            </p>
             <div class="btns">
                 <button class="specs-page">
-                    <a href="specs.php">Specs</a>
+                    <a href="#">Specs</a>
                 </button>
                 <button class="promotie-page">
-                    <a href="promotie.php">Promotie</a>
+                    <a href="#">Promotie</a>
                 </button>
             </div>
         </div>
