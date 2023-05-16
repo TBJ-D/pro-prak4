1e8d064c7852139ccce1efec15ba1fd9cd78bc4a
Igone van_der_Kley <igone@vdkley.nl>
2023-05-16 08:36:49 +0200

    promotie.php begonnen

---
+++ promotie.php
@@ -0,0 +1,62 @@
+<head>
+    <link rel="stylesheet" href="/scss/promotie.scss">
+</head>
+
+<body>
+    <?php include_once("header.php"); ?>
+
+   
+
+    <div class="promo-content">
+        <div class="banner-picture">
+            <img src="media/promo_banner.png" alt="phone">
+        
+
+
+        <h1>Introducing the Iphone 14</h1>
+    </div>
+
+        <div class="promo-tekst">
+
+
+            <img class="round" src="/media/iphone_hands_promo.png" alt="">
+
+            <p>Introducing the iPhone 14 - the ultimate smartphone experience that sets the bar for the future of mobile technology.
+                With groundbreaking advancements in design, performance, and user experience,
+                the iPhone 14 is truly the best iPhone yet.
+            </p>
+
+            <p>
+
+                At the heart of the iPhone 14 is the new A18 Bionic chip, which is faster, smarter, and more efficient than ever before.
+                With this cutting-edge chip, you can enjoy lightning-fast speeds, seamless multitasking, and improved battery life,
+                making it the perfect device for all your daily needs.
+            </p>
+            <p>
+
+                The iPhone 14 also boasts a stunning Super Retina XDR display that showcases vibrant colors, deep blacks,
+                and an immersive viewing experience like never before. With its edge-to-edge design,
+                the screen stretches from edge to edge, providing a larger canvas for all your photos, videos, and apps.
+                The iPhone 14's camera system has been reimagined to capture every moment with stunning detail and clarity.
+                With its advanced AI capabilities, you can take incredible photos and videos with ease.
+                Whether you're a professional photographer or just love snapping photos on the go,
+                the iPhone 14's camera will exceed your expectations.
+
+            </p>
+            <p>
+                Additionally, the iPhone 14 has been designed to be more durable than ever before,
+                with a Ceramic Shield front cover that provides unparalleled protection against drops and scratches. With its water-resistant design,
+                you can also take your iPhone 14 with you wherever you go, without worrying about damage from water or dust.
+
+                The iPhone 14 also features 5G connectivity, which provides lightning-fast speeds for streaming, gaming, and downloading.
+                With its sleek and stylish design,
+                the iPhone 14 is the perfect blend of form and function, providing a seamless user experience that will leave you in awe.
+
+                Don't miss out on the opportunity to experience the future of mobile technology.
+                Get your hands on the iPhone 14 today and discover what true innovation looks like.
+            </p>
+        </div>
+    </div>
+    </main>
+
+</body>
